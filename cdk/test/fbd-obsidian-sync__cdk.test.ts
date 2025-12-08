a542766a7be000284603667e968081eac4ea0c40
FBD <fajarletters@gmail.com>
2025-12-08 08:48:35 -0800

    added cdk

---
+++ cdk/test/fbd-obsidian-sync__cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FbdObsidianSyncCdk from '../lib/fbd-obsidian-sync__cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fbd-obsidian-sync__cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FbdObsidianSyncCdk.FbdObsidianSyncCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
