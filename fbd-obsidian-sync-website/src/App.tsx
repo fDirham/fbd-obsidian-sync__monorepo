2104e043935f7327746a11e8efe8388cc0c687a6
FBD <fajarletters@gmail.com>
2025-12-08 15:01:46 -0800

    basic website

--- fbd-obsidian-sync-website/src/App.tsx
+++ fbd-obsidian-sync-website/src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="hero">
+        <h1 className="hero__title">FBD Obsidian Sync</h1>
+        <p className="hero__blurb">
+          An open source cross-platform syncing plugin for{" "}
+          <a href="https://obsidian.md/" target="_blank">
+            Obsidian.md
+          </a>
+        </p>
+        <div className="hero__actions">
+          <a
+            href="https://github.com/fDirham/fbd-obsidian-sync__plugin"
+            target="_blank"
+          >
+            Plugin
+          </a>
+          <a
+            href="https://github.com/fDirham/fbd-obsidian-sync__monorepo"
+            target="_blank"
+          >
+            Backend
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="faq">
+        <h2>FAQ</h2>
+        <h3>How do I use this?</h3>
+        <ol>
+          <li>Install the plugin (works on mobile too!).</li>
+          <li>Open the plugin settings and create an account or log in.</li>
+          <li>Create a new "vault" inside the plugin settings.</li>
+          <li>Click upload to create a backup.</li>
+          <li>Click restore to download a backup.</li>
+        </ol>
+        <h3>How does syncing work?</h3>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          The plugin reads all files in your vault (not including .obsidian),
+          zips it, and uploads it to the cloud. When you want to sync, it
+          retrieves said files and rebuilds your entire vault.
+        </p>
+        <p>
+          Future versions will be more complex by calculating changesets between
+          syncs to allow for auto syncing, but this basic logic is good enough.
+        </p>
+        <h3>What was this built with?</h3>
+        <p>
+          <ul>
+            <li>The front end is just a pure Obsidian plugin.</li>
+            <li>
+              The backend is a Java Springboot API hosted in an AWS lambda. Also
+              use DynamoDB, S3, Cognito, and API Gateway.
+            </li>
+          </ul>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
