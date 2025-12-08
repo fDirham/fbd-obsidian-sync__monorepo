6cd8485f67f1a136137a9dede9d464ab650a39d1
FBD <fajarletters@gmail.com>
2025-12-08 09:52:45 -0800

    simple lambda deploy

--- cdk/lib/fbd-obsidian-sync__cdk-stack.ts
+++ cdk/lib/fbd-obsidian-sync__cdk-stack.ts
@@ -1,16 +1,34 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as path from "path";
 
 export class FbdObsidianSyncCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const springBootLambda = new lambda.Function(this, "SpringBootFunction", {
+      runtime: lambda.Runtime.JAVA_21, // Wow
+      handler:
+        "com.fbdco.fbd_obsidian_sync_dashboard_api.LambdaHandler::handleRequest",
+      code: lambda.Code.fromAsset(
+        path.join(
+          __dirname,
+          "../../dashboard-api-lambda/.aws-sam/build/SpringBootFunction"
+        )
+      ),
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(30),
+      environment: {
+        SPRING_PROFILES_ACTIVE: "prod",
+        // Add your environment variables
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FbdObsidianSyncCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new apigateway.LambdaRestApi(this, "SpringBootApi", {
+      handler: springBootLambda,
+      proxy: true,
+    });
   }
 }
